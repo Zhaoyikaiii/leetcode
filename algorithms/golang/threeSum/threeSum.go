1631a275b4d801e963149263cfc9bff3d15564c7
zhaoyikaii <stryhytrgb@gmail.com>
2024-03-05 22:37:40 +0800

    New Solution - '15.3Sum'

---
+++ algorithms/golang/threeSum/threeSum.go
@@ -0,0 +1,30 @@
+package threesum
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	res := [][]int{}
+	for i := range nums {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		l, r := i+1, len(nums)-1
+		for l < r {
+			s := nums[i] + nums[l] + nums[r]
+			if s < 0 {
+				l++
+			} else if s > 0 {
+				r--
+			} else {
+				res = append(res, []int{nums[i], nums[l], nums[r]})
+				l++
+				for l < r && nums[l] == nums[l-1] {
+					l++
+				}
+
+			}
+		}
+	}
+	return res
+}
